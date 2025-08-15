edfd5ba51754c4b19909522a135e55f996d62104
DombSTG <viniciusdombidau@gmail.com>
2025-08-15 09:34:38 -0300

    Adicionado array global e verificação de erros

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
+let numarr = []
+
 function numero () {
     let nu = Number(document.getElementById('num').value)
     let res = document.getElementById('res')
-    let numarr = [nu]
+    let numero = document.getElementById('numero')
 
     if (nu < 1 || nu > 100) {
         alert('[ERRO] Valor Inválido!')
-    } else if (numarr.indexOf(nu) != -1) {
+    } else if (numarr.indexOf(nu) === -1) {
         numarr.push(nu)
         let item = document.createElement ('option')
         item.text = `Valor ${nu} adicionado`
+        numero.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Valor já adicionado anteriormente')
     }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
